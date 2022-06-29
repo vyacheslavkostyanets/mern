61674b0770c518c0034c3d4cae5d61a82e831521
Vichaslav Kostianets <vyacheslav.kostyanets@gmail.com>
2022-06-29 23:51:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("huilo ");
+})
+
+app.listen(port, () => {
+	console.log(`server ran ${port}`)
+})
